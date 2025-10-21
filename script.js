8b7f85cf37d72b55c6f4a9d2bc22a5d894cb4037
Marcin Wiśniowski <149424888+rendor535@users.noreply.github.com>
2025-10-21 08:49:24 +0000

    Com2_1.1

--- script.js
+++ script.js
@@ -14,28 +14,28 @@
       })
   })
 
-  cw1.addEventListener("click", function () {
+  cw1.addEventListener("click", function() {
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(posts => {
-      let html = '<h2>Lista postów</h2>';
-      html += '<ul style="list-style:none; padding:0;">';
-      posts.forEach(post => {
+      .then(response => response.json())
+      .then(posts => {
+        let html = '<h2>Lista postów</h2>';
+        html += '<ul style="list-style:none; padding:0;">';
+        posts.forEach(post => {
           html += `
             <li style="margin-bottom:15px; border-bottom:1px solid #ccc; padding-bottom:10px;">
-              <st rong>${post.id}. ${post.title}</strong><br>
+              <strong>${post.id}. ${post.title}</strong><br>
               <em>userId: ${post.userId}</em><br>
               <p>${post.body}</p>
             </li>
           `;
         });
-      html += '</ul>';
-      answer.innerHTML = html;
-    })
-    .catch(error => {
-      console.error(error);
-      answer.innerHTML = '<p>Błąd podczas pobierania danych.</p>';
-    });
+        html += '</ul>';
+        answer.innerHTML = html;
+      })
+      .catch(error => {
+        console.error(error);
+        answer.innerHTML = '<p>Błąd podczas pobierania danych :(.</p>';
+      });
   });
 
   cw2.addEventListener("click", function() {
